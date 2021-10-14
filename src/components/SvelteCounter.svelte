152a580d0bec5c67ca69d716e62951e2314a547f
Felipe300 <caracoloro@gmail.com>
2021-10-14 19:07:44 -0300

    Adding Svelte Counter

---
+++ src/components/SvelteCounter.svelte
@@ -0,0 +1,14 @@
+<script>
+    let count = 0;
+
+    const add = () => count++
+    const subtract = () => count--
+    const reset = () => count = 0
+</script>
+
+<div>
+    <h2>Svelte count = {count}</h2>
+    <button on:click={add}>+</button>
+    <button on:click={subtract}>-</button>
+    <button on:click={reset}>reset</button>
+</div>
